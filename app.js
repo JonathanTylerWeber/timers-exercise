e57dbaa4961ed0c12b3f44740efef4b265efa70f
JonathanTylerWeber <jonathantweber@gmail.com>
2023-08-02 01:29:51 +0800

    random game done

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-function countdown (num) {
+// function countdown (num) {
+//     let intervalId = setInterval(function(){
+//         num--;
+//         console.log(num);
+//     if (num === 1){
+//         setTimeout(function() {
+//             console.log('DONE!')}
+//             , 1000)
+//         clearInterval(intervalId);
+//     }    
+//     },
+//     1000
+//     )
+// }
+
+function randomGame(){
+    let counter = 0;
     let intervalId = setInterval(function(){
-        num--;
-        console.log(num);
-    if (num === 1){
-        setTimeout(function() {
-            console.log('DONE!')}
-            , 1000)
+        let x = Math.random();
+        console.log(`Number = ${x}`);
+        counter++;
+        console.log(`Try #${counter}`);
+    if (x >= .75){
         clearInterval(intervalId);
-    }    
-    },
-    1000
+    }
+    }
+    ,1000
     )
 }
